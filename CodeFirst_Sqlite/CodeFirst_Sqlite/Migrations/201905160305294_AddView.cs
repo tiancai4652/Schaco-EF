2cd06d22d8603b70f8ffe4488dfd3a273736a4f2
Shaco <zhangranmm@126.com>
2019-05-16 11:06:30 +0800

    Add View Come True

---
+++ CodeFirst_Sqlite/CodeFirst_Sqlite/Migrations/201905160305294_AddView.cs
@@ -0,0 +1,25 @@
+﻿namespace CodeFirst_Sqlite.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddView : DbMigration
+    {
+        public override void Up()
+        {
+            string script =
+        @"
+        CREATE VIEW [StudentWeaponView]
+        AS SELECT p.ID AS StudentID, p.Name AS StudentName,u.ID AS WeaponID,u.Name AS WeaponName
+        FROM [Students] p
+        INNER JOIN [Weapon] u ON u.Id = p.Id";
+            DBContext.DBContext ctx = new DBContext.DBContext();
+            ctx.Database.ExecuteSqlCommand(script);
+        }
+
+        public override void Down()
+        {
+            DropTable("dbo.StudentWeaponView");
+        }
+    }
+}
